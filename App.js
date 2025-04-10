eb79e12138bf82606227d2139442ad46945227a6
unknown <sidritmucaj@domos.al>
2025-04-10 19:25:56 +0200

    Initial commit

---
+++ App.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, SafeAreaView, ScrollView, Image, TouchableOpacity } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Ionicons } from '@expo/vector-icons';
+
+export default function App() {
+  return (
+    <>
+    <h1>Helloooooooooooooooooooooooooooo</h1>
+    </>
+  );
+};
